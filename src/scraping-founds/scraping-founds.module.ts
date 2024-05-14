870aefe1a3bb1937c8d9187018e0e8401278e8b2
Ivan Orlando Diaz <iodiaz@co.acfspain.org>
2024-05-14 10:45:52 -0500

    se ajusta la matriz de fuentes lite para ignorar proceso de scrapin y creacion de proceso de scraping

--- src/scraping-founds/scraping-founds.module.ts
+++ src/scraping-founds/scraping-founds.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScrapingFoundsController } from '../scraping-founds/scraping-founds.controller';
 import { ScrapingFoundsService } from '../scraping-founds/scraping-founds.service';
 import { HttpModule } from '@nestjs/axios'
+import { ConvertToExcelService } from '../services/convert-to-excel/convert-to-excel.service';
 
 @Module({
     controllers: [ScrapingFoundsController],
-    providers: [ScrapingFoundsService],
+    providers: [ScrapingFoundsService, ConvertToExcelService],
     imports: [HttpModule],
 })
 export class ScrapingFoundsModule { }
